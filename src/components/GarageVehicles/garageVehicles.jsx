1d0620ffe90ab2e3a63beaab49cb7053beeb44c8
Your Name <charles.luas.young@gmail.com>
2021-12-21 13:12:18 -0700

    Load operator vehicle card

--- src/components/GarageVehicles/garageVehicles.jsx
+++ src/components/GarageVehicles/garageVehicles.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react';
-import { Container, Card, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Container, Card, Button, CardGroup, CardImg } from 'react-bootstrap';
 import axios from 'axios';
 
 const GarageVehicles = (props) => {
-    const [vehicles, setVehicles] = useState("");
+    const [vehicles, setVehicles] = useState([]);
+
+    useEffect(() => {
+        getVehicles()
+    }, [])
 
     const getVehicles = async () => {
         const jwt = localStorage.getItem('token')
         let response = await axios.get('http://127.0.0.1:8000/api/operator_vehicle/garage/', {headers: {Authorization: 'Bearer ' + jwt}})
         console.log(response.data)
-        // if (response.request.status === 201){
-            
-        // }
+        setVehicles(response.data)
     };
 
     return (
         <Container>
             <>
-            <Card style={{width: '18rem'}}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>Vehicle description</Card.Text>
-                    <Button variant="primary">More Info</Button>
-                    <Button variant="primary">Maintenance</Button>
-                </Card.Body>
-            </Card>
+              <CardGroup>
+                {vehicles.map((e) =>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>{e.make} + {e.model}</Card.Title>
+                            <Card.Text>{e.drive_type}</Card.Text>
+                            <Button variant="primary">More Info</Button>
+                            <Button variant="primary">Maintenance</Button>
+                        </Card.Body>
+                    </Card>
+                )}
+              </CardGroup>    
             </>
         </Container>
 
