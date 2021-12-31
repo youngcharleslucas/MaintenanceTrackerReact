1e7641774631028e73b76ac863d45cfb70dab2de
Your Name <charles.luas.young@gmail.com>
2021-12-31 10:16:28 -0700

    Display 3 types of alerts, non function button

--- src/components/Alerts/alerts.jsx
+++ src/components/Alerts/alerts.jsx
@@ -19,14 +19,32 @@ const AlertsPage = (props) => {
     };
 
     return (
+        // <>
+        // <h1>Alerts</h1>
+        //     <h2>{vehicle_id}</h2>
+        //     <Container>
+        //         <h1>Overdue</h1>                   
+        //             {alert_list.map((e) =>
+        //                 <Row>
+        //                     <Alert variant="danger">
+        //                         <Alert.Heading>{e.maintenance.maintenance_name}</Alert.Heading>
+        //                         <hr />
+        //                         <p className="mb-0">Due: {e.log_miles + e.maintenance.maintenance_miles}</p>
+        //                         <div className="d-flex justify-content-end">
+        //                             <Button variant="danger">Complete</Button>
+        //                         </div>
+        //                     </Alert>               
+        //                 </Row>
+        //             )}
+        //     </Container>
+        // </>
         <>
             <h1>Alerts</h1>
             <h2>{vehicle_id}</h2>
             <Container>
-                <Row> 
-                    <h1>Overdue</h1>                   
-                    {alert_list.map((e) =>
-                        <Col>
+                <h1>Overdue</h1>                   
+                    {alert_list.filter((filtered) => filtered.vehicle.miles_current > (filtered.log_miles + filtered.maintenance.maintenance_miles)).map((e) =>
+                        <Row>
                             <Alert variant="danger">
                                 <Alert.Heading>{e.maintenance.maintenance_name}</Alert.Heading>
                                 <hr />
@@ -35,11 +53,35 @@ const AlertsPage = (props) => {
                                     <Button variant="danger">Complete</Button>
                                 </div>
                             </Alert>               
-                        </Col>
+                        </Row>
                     )}
-                </Row>
-
-            </Container>
+                <h1>Approaching</h1>                   
+                    {alert_list.filter((filtered) => (filtered.vehicle.miles_current + 500) > (filtered.log_miles + filtered.maintenance.maintenance_miles) && (filtered.vehicle.miles_current) < (filtered.log_miles + filtered.maintenance.maintenance_miles) ).map((e) =>
+                        <Row>
+                            <Alert variant="warning">
+                                <Alert.Heading>{e.maintenance.maintenance_name}</Alert.Heading>
+                                <hr />
+                                <p className="mb-0">Due: {e.log_miles + e.maintenance.maintenance_miles}</p>
+                                <div className="d-flex justify-content-end">
+                                    <Button variant="warning">Complete</Button>
+                                </div>
+                            </Alert>               
+                        </Row>
+                    )}
+                <h1>Scheduled</h1>                   
+                    {alert_list.filter((filtered) => (filtered.vehicle.miles_current + 500) < (filtered.log_miles + filtered.maintenance.maintenance_miles) ).map((e) =>
+                        <Row>
+                            <Alert variant="success">
+                                <Alert.Heading>{e.maintenance.maintenance_name}</Alert.Heading>
+                                <hr />
+                                <p className="mb-0">Due: {e.log_miles + e.maintenance.maintenance_miles}</p>
+                                <div className="d-flex justify-content-end">
+                                    <Button variant="success">Complete</Button>
+                                </div>
+                            </Alert>               
+                        </Row>
+                    )}
+            </Container> 
         </>
     );
 };
